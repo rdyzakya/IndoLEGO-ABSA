8763f5a82dd9dc4519a29534d1198c1aa8a08905
Randy Zakya Suchrady <randy.suchrady@dgx-2.prosa.internal>
2022-10-19 10:37:34 +0700

    experiment mt5

--- gabsa/gpu.py
+++ gabsa/gpu.py
@@ -1,5 +1,4 @@
 from pynvml import *
-import torch
 
 
 def print_gpu_utilization(index):
@@ -9,5 +8,10 @@ def print_gpu_utilization(index):
     print(f"GPU (index : {index}) memory occupied: {info.used//1024**2} MB.")
 
 def print_all_gpu_utilization():
-    for i in range(torch.cuda.device_count()):
-        print_gpu_utilization(i)
+    i = 0
+    while True:
+        try:
+            print_gpu_utilization(i)
+            i += 1
+        except:
+            return
--- gabsa/scripts/train.sh
+++ gabsa/scripts/train.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=1,3 python main.py --do_train \
+CUDA_VISIBLE_DEVICES=3 python main.py --do_train \
             --do_eval \
             --do_predict \
             --train_args /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/train_args/train_args.json \
             --model_type mt5 \
             --model_name_or_path munggok/mt5-translate-en-id \
             --max_len 256 \
-            --task "aste" \
+            --task "aste ate ote aope uabsa" \
             --paradigm extraction \
             --prompt_option 0 \
             --prompt_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/prompt.json \
             --prompt_option_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/option.json \
             --pattern /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/patterns/default.json \
             --data_dir /srv/nas_data1/text/randy/absa/facebook-absa/data/interim/gaste_format \
-            --trains "train_news_annotator" \
+            --trains "train_socmed_twenty_percent train_news_annotator train_news_student" \
             --devs "test_news" \
             --tests "test_news" \
             --blank_frac 0.0 \
