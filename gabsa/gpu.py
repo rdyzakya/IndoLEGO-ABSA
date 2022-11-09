12b5d8c4ff216541090056232df3c1309ae5ce53
Randy Zakya Suchrady <randy.suchrady@dgx-2.prosa.internal>
2022-11-10 00:07:55 +0700

    restruct dataset

--- gabsa/gpu.py
+++ gabsa/gpu.py
@@ -5,13 +5,14 @@ def print_gpu_utilization(index):
     nvmlInit()
     handle = nvmlDeviceGetHandleByIndex(index)
     info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU (index : {index}) memory occupied: {info.used//1024**2} MB.")
+    return info.used//1024**2
 
 def print_all_gpu_utilization():
     i = 0
     while True:
         try:
-            print_gpu_utilization(i)
+            used = print_gpu_utilization(i)
+            print(f"GPU (index : {i}) memory occupied: {used//1024**2} MB.")
             i += 1
         except:
             return
