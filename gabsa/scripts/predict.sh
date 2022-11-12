4106fa17bedeb39585e201214dda80d5d0b92825
Randy Zakya Suchrady <randy.suchrady@dgx-2.prosa.internal>
2022-11-12 08:02:20 +0700

    error analysis

--- gabsa/scripts/predict.sh
+++ gabsa/scripts/predict.sh
@@ -3,16 +3,16 @@
 CUDA_VISIBLE_DEVICES=3 python /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/main.py --do_predict \
             --train_args /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/train_args/train_args.json \
             --model_type t5 \
-            --model_name_or_path /srv/nas_data1/text/randy/absa/models/facebook_research/generative/fix/t5/t5_pabsa_S256_wid_small_blank=1.0 \
+            --model_name_or_path /srv/nas_data1/text/randy/absa/models/facebook_research/generative_unprompted/fix/t5/t5_aste_S256_wid_small_blank=1.0 \
             --max_len 256 \
             --task "aste" \
             --paradigm extraction \
             --prompt_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/prompt.json \
-            --prompt_option_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/option.json \
+            --prompt_option_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/option_no_prompt.json \
             --pattern /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/patterns/default.json \
             --data_dir /srv/nas_data1/text/randy/absa/facebook-absa/data/combination/prosa/gaste_format \
-            --tests "test_socmed" \
+            --tests "test_news" \
             --blank_frac 1.0 \
             --random_state 17 \
-            --output_dir /srv/nas_data1/text/randy/absa/models/facebook_research/generative/fix/t5/t5_pabsa_S256_wid_small_blank=1.0/aste/test_socmed \
+            --output_dir /srv/nas_data1/text/randy/absa/models/facebook_research/generative_unprompted/fix/t5/aste/test_news \
             --per_device_predict_batch_size 32
--- gabsa/scripts/train.sh
+++ gabsa/scripts/train.sh
@@ -7,10 +7,10 @@ CUDA_VISIBLE_DEVICES=2 python /srv/nas_data1/text/randy/absa/facebook-absa/gabsa
             --model_type t5 \
             --model_name_or_path Wikidepia/IndoT5-small \
             --max_len 256 \
-            --task "aste ate ote uabsa aope" \
+            --task "aste" \
             --paradigm extraction \
             --prompt_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/prompt.json \
-            --prompt_option_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/option.json \
+            --prompt_option_path /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/prompts/option_no_prompt.json \
             --pattern /srv/nas_data1/text/randy/absa/facebook-absa/gabsa/patterns/default.json \
             --data_dir /srv/nas_data1/text/randy/absa/facebook-absa/data/combination/prosa/gaste_format \
             --trains "train_news train_socmed val_news val_socmed" \
@@ -18,7 +18,7 @@ CUDA_VISIBLE_DEVICES=2 python /srv/nas_data1/text/randy/absa/facebook-absa/gabsa
             --tests "test_news test_socmed" \
             --blank_frac 1.0 \
             --random_state 17 \
-            --output_dir /srv/nas_data1/text/randy/absa/models/facebook_research/generative/fix \
+            --output_dir /srv/nas_data1/text/randy/absa/models/facebook_research/generative_unprompted/fix \
             --per_device_predict_batch_size 32
             # --trains "train_news_annotator train_news_student train_socmed_HaloBCA train_socmed_Telmark train_socmed_V3_sentiment" \
             # --task "aste aope ate ote uabsa" \
