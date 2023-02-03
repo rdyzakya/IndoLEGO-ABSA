c630e230a9575a9af50b8b1dc2588aa022837f2e
rdyzakya <randomzakya@gmail.com>
2023-02-03 10:25:37 +0700

    create: add constant.py ; update: add method process num targets for dataset.py and move constanst to constant.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,11 @@
 from datasets import Dataset, DatasetDict
+from pattern import Pattern
+from prompt import Prompter
 import pandas as pd
 
 from typing import Dict, List
+
+from constant import SEP, SENTTAG2WORD, SENTIMENT_ELEMENT
 
 # THINGS TO DO
 # a. Consider tasks
@@ -10,14 +14,13 @@ from typing import Dict, List
 # d. Consider extraction & imputation
 # e. Multiply the data or not
 
-SEP = "####"
 sample = "It rarely works and when it does it 's incredibly slow .####[([2], [1], 'NEG')]"
 
 class ABSADataset(Dataset):
     """
     ABSA Dataset.
     """
-    def __init__(self,data_path:str,target_format:str,tasks:Dict[str,List],multiply:bool=True,shuffle:bool=True,random_state:int=None):
+    def __init__(self,data_path:str,target_format:str="acos",tasks:Dict[str,List]={"extraction" : ["ao","ac","as"],"imputation" : ["acos"]},multiply:bool=True,shuffle:bool=True,random_state:int=None):
         """
         ### DESC
             Constructor method for ABSA dataset.
@@ -50,7 +53,42 @@ class ABSADataset(Dataset):
             except Exception as e:
                 raise ValueError(f"Each line should be in the format 'TEXT{SEP}TARGET. Example: {sample}'")
             data[i] = {"text" : text, "num_targets" : num_targets}
+        
+        # Is multiply or not
+        new_data = []
+        if multiply:
+            for row in data:
+                pass
+    
+    def process_num_targets(self,text:str,num_targets:List[tuple],task:str) -> List[Dict]:
+        """
+        ### DESC
+            Method for processing num targets to target in the format list of dictionaries.
+        ### PARAMS
+        * text: Text source.
+        * num_targets: Targets in the form list of tuples, may consist of aspect term or opinion term indexes.
+        * task: The designated task. Example: axos, aos, ac, ao, etc.
+        ### RETURN
+        * result_targets: The resultant targets in the form list of dictionaries.
+        """
+        splitted_text = text.split()
+        result_targets = []
+        for num_target in num_targets:
+            assert len(num_target) == len(task) # number of element in the num targets must be the same with the task
+            target = {}
+            for i, se in enumerate(task): # iterate a, c, o, s
+                assert se in 'acos'
+                key = SENTIMENT_ELEMENT[se]
+                if se == 'a' or se == 'o':
+                    value = ' '.join([splitted_text[j] for j in num_target[i]])
+                elif se == 's':
+                    value = SENTTAG2WORD[num_target[i]]
+                else: # se == 'c
+                    value = num_target[i]
+                target[key] = value
+            result_targets.append(target)
+        return result_targets
 
-
-a = ABSADataset('test.txt',{'imputation' : ['ao']})
-ABSADataset
+if __name__ == "__main__":
+    a = ABSADataset('test.txt',{'imputation' : ['ao']})
+    ABSADataset
--- data/pattern.py
+++ data/pattern.py
@@ -1,12 +1,5 @@
 from typing import List, Dict
-
-SENTTAG2WORD = {'POS': 'positive', 'NEG': 'negative', 'NEU': 'neutral'}
-SPECIAL_CHAR = {"[": "\[", "]": "\]", ".": "\.", "\\": "\\", "{": "\{", "}": "\}", "^": "\^", "$": "\$", "*": "\*", "+": "\+", "?": "\?", "|": "\|", "(": "\(", ")": "\)"}
-PATTERN_TOKEN = {"aspect" : "<A>", "opinion" : "<O>", "sentiment" : "<S>", "category" : "<C>"}
-SENTIMENT_ELEMENT = {'a' : "aspect", 'o' : "opinion", 's' : "sentiment", 'c' : "category"}
-# NOTE FOR SPECIAL_CHAR:
-# // symbol is the same, because the code use replace method with regex parameter set to True
-# result example: noodle/spaghetti --> noodle//spaghetti
+from constant import SENTTAG2WORD, SPECIAL_CHAR, PATTERN_TOKEN, SENTIMENT_ELEMENT
 
 class Pattern:
     """
